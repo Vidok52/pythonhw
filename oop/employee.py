1a32d14b24b96c4b2fd81eca5f8b5bf8ec77d06d
Vidok52 <svets84@gmail.com>
2018-11-19 20:30:18 +0200

    Add files via upload

---
+++ oop/employee.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, jsonify
+import pymysql
+
+app = Flask(__name__)
+
+
+class Database:
+    def __init__(self):
+        host = "127.0.0.1"
+        user = "maks"
+        password = "12345"
+        db = "employees"
+
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
+
+    def list_employees(self):
+        self.cur.execute("SELECT first_name, last_name, gender FROM employees LIMIT 50")
+        result = self.cur.fetchall()
+
+        return result
+
+@app.route('/')
+def employees():
+
+    def db_query():
+        db = Database()
+        emps = db.list_employees()
+
+        return emps
+
+    res = db_query()
+
+    return jsonify(res) #render_template('employees.html', result=res, content_type='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
